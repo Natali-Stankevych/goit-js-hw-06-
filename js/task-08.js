11a9237c3204e0dbda72b6b407e4b56f22b160cb
Natali-Stankevych <110481629+Natali-Stankevych@users.noreply.github.com>
2022-12-10 20:29:54 +0200

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -9,7 +9,7 @@ function handSabmat(evt) {
     if (!email.value || !password.value) {
         return alert('всі поля повинні бути заповнені');
     }
-};
-console.log(`Email:#${email.value},
-Password: ${password.value}`);
+
+console.log(`Email:${email.value}, Password: ${password.value}`);
 evt.currentTarget.reset();
+};
